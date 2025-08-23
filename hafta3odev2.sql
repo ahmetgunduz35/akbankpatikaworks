ee0e0b3373b1469dc37bb50196ffff2461e61c46
Ahmet Gündüz <70411203+ahmetgunduz35@users.noreply.github.com>
2025-08-23 22:03:19 +0300

    Update hafta3odev2.sql

--- hafta3odev2.sql
+++ hafta3odev2.sql
@@ -1,18 +1,18 @@
-1. film tablosunda bulunan tüm sütunlardaki verileri replacement_cost değeri 12.99’dan büyük eşit ve 16.99’dan küçük olma koşuluyla (BETWEEN - AND):
+-- 1) film tablosunda bulunan tüm sütunlardaki verileri replacement_cost değeri 12.99’dan büyük eşit ve 16.99’dan küçük olma koşuluyla (BETWEEN - AND):
 
 SELECT *
 FROM film
 WHERE replacement_cost BETWEEN 12.99 AND 16.99;
 
 
-2. actor tablosunda bulunan first_name ve last_name sütunlarındaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' olanları getir (IN kullanarak):
+-- 2) actor tablosunda bulunan first_name ve last_name sütunlarındaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' olanları getir (IN kullanarak):
 
 SELECT first_name, last_name
 FROM actor
 WHERE first_name IN ('Penelope', 'Nick', 'Ed');
 
 
-3. film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla (IN kullanarak):
+-- 3) film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma koşullarıyla (IN kullanarak):
 
 SELECT *
 FROM film
